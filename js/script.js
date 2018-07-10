16b5c720bb5fec0d1178abe2134299bdfee12a07
Md Mizanur Rahman <mdmizanur3950@gmail.com>
2018-07-10 22:09:57 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('body').scrollspy({ target: '#main-nav'});
+
+$('#main-nav a').on('click', function(e){
+    if(this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900,function(){
+            window.location.hash=hash;
+        });
+    }
+});
